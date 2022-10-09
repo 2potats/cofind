a4860d1db691db35ff2a6d1dbf0788ef39ae0745
jmeshen <hello@jmeshen.com>
2022-10-08 22:58:27 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="landing page" />
+</svelte:head>
+
+<section>
+	<div class="bg"></div>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	.bg {
+		background: url('pkmn-meowth-love.webp');
+	}
+</style>
---
+++ src/routes/guides/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
+
+<h1>{data.title}</h1>
+<div class="text-3xl font-bold underline">{@html data.content}</div>
